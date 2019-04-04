31e79387b121efea1aca17c296b09c6c2f7c3ac3
Mike De Groot <mdegroot09@gmail.com>
2019-04-03 22:02:50 -0600

    added edit and new button features

--- src/App.js
+++ src/App.js
@@ -8,17 +8,100 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-      data: data
+      data: data,
+      currentIndex: 0
     }
   }
   
   delete = (currentIndex) => {
     let data = this.state.data
     data.splice(currentIndex,1);
-    if (currentIndex === this.state.data.length){
+    if (currentIndex === data.length){
       this.setState({currentIndex: 0})
     }
+    if (data.length === 0){
+      data.push({
+        id: 'Nada',
+        name: { first: 'No Current Users', last: ''},
+        city: 'Notown',
+        country: 'Nowhere',
+        employer: 'Nobody',
+        title: 'Nothing',
+        favoriteMovies: ['Devils', 'Wear', 'Nada']
+      })
+    }
     this.setState({data: data})
+  }
+
+  editUser = (currentIndex, editOrSave) => {
+    if (editOrSave === 'Edit'){
+      document.getElementById('changeFirstName').innerHTML = `<input id='inputFirstName' value='${this.state.data[currentIndex].name.first}'>`
+      document.getElementById('changeLastName').innerHTML = `<input id='inputLastName' value='${this.state.data[currentIndex].name.last}'>`
+      document.getElementById('changeCity').innerHTML = `<input id='inputCity' value='${this.state.data[currentIndex].city}'>`
+      document.getElementById('changeCountry').innerHTML = `<input id='inputCountry' value='${this.state.data[currentIndex].country}'>`
+      document.getElementById('changeTitle').innerHTML = `<input id='inputTitle' value='${this.state.data[currentIndex].title}'>`
+      document.getElementById('changeEmployer').innerHTML = `<input id='inputEmployer' value='${this.state.data[currentIndex].employer}'>`
+      document.getElementById('changeMovie1').innerHTML = `<input id='inputMovie1' value='${this.state.data[currentIndex].favoriteMovies[0]}'>`
+      document.getElementById('changeMovie2').innerHTML = `<input id='inputMovie2' value='${this.state.data[currentIndex].favoriteMovies[1]}'>`
+      document.getElementById('changeMovie3').innerHTML = `<input id='inputMovie3' value='${this.state.data[currentIndex].favoriteMovies[2]}'>`
+      document.getElementById('editSave').innerHTML = `<span className='edit'>Save</span>`
+    } else {
+      let data = this.state.data
+      data[currentIndex].name.first = document.getElementById('inputFirstName').value
+      document.getElementById('changeFirstName').innerHTML = `${data[currentIndex].name.first}`
+      data[currentIndex].name.last = document.getElementById('inputLastName').value
+      document.getElementById('changeLastName').innerHTML = `${data[currentIndex].name.last}`
+      data[currentIndex].city = document.getElementById('inputCity').value
+      document.getElementById('changeCity').innerHTML = `${data[currentIndex].city}`
+      data[currentIndex].country = document.getElementById('inputCountry').value
+      document.getElementById('changeCountry').innerHTML = `${data[currentIndex].country}`
+      data[currentIndex].title = document.getElementById('inputTitle').value
+      document.getElementById('changeTitle').innerHTML = `${data[currentIndex].title}`
+      data[currentIndex].employer = document.getElementById('inputEmployer').value
+      document.getElementById('changeEmployer').innerHTML = `${data[currentIndex].employer}`
+      data[currentIndex].favoriteMovies[0] = document.getElementById('inputMovie1').value
+      document.getElementById('changeMovie1').innerHTML = `${data[currentIndex].favoriteMovies[0]}`
+      data[currentIndex].favoriteMovies[1] = document.getElementById('inputMovie2').value
+      document.getElementById('changeMovie2').innerHTML = `${data[currentIndex].favoriteMovies[1]}`
+      data[currentIndex].favoriteMovies[2] = document.getElementById('inputMovie3').value
+      document.getElementById('changeMovie3').innerHTML = `${data[currentIndex].favoriteMovies[2]}`
+      document.getElementById('editSave').innerHTML = `<span className='edit'>Edit</span>`
+      console.log('data:',data[currentIndex])
+    }
+    this.setState({data: data})
+  }
+
+  newUser = (currentIndex, editOrSave) => {
+    if(editOrSave !== 'Save'){
+      let data = this.state.data
+      let tempUser = {
+        id: 'new',
+        name: { first: '', last: ''},
+        city: '',
+        country: '',
+        employer: '',
+        title: '',
+        favoriteMovies: ['', '', '']
+      }
+      data.splice(currentIndex,0,tempUser)
+      this.setState({data: data})
+    }
+  }
+
+  incrementIndex = (val) => {
+    if (val + 1 < this.state.data.length) {
+      this.setState({currentIndex: val + 1})
+    } else {
+      this.setState({currentIndex: 0})
+    }
+  }
+
+  decrementIndex = (val) => {
+    if (val - 1 >= 0) {
+      this.setState({currentIndex: val - 1})
+    } else {
+      this.setState({currentIndex: this.state.data.length - 1})
+    }
   }
 
   render() {
@@ -27,7 +110,13 @@ class App extends Component {
         <Header/>
         <DataBox 
           data={this.state.data} 
-          delete={this.delete}/>
+          delete={this.delete}
+          incrementIndex={this.incrementIndex}
+          decrementIndex={this.decrementIndex}  
+          currentIndex={this.state.currentIndex}
+          newUser={this.newUser}
+          editUser={this.editUser}
+        />
       </div>
     );
   }
--- src/DataBox.jsx
+++ src/DataBox.jsx
@@ -2,46 +2,23 @@ import React, {Component} from 'react'
 import './DataBox.css'
 
 export default class DataBox extends Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      currentIndex: 0
-    }
-  }
-
-  incrementIndex = (val) => {
-    if (val + 1 < this.props.data.length) {
-      this.setState({currentIndex: val + 1})
-    } else {
-      this.setState({currentIndex: 0})
-    }
-  }
-  
-  decrementIndex = (val) => {
-    if (val - 1 >= 0) {
-      this.setState({currentIndex: val - 1})
-    } else {
-      this.setState({currentIndex: this.props.data.length - 1})
-    }
-  }
-
   render(){
-    let movieList = this.props.data[this.state.currentIndex].favoriteMovies.map((movie, i) => {
-        return <p className='dataLine' key={i}>{i + 1}. {movie}</p>
-      })
+    let movieList = this.props.data[this.props.currentIndex].favoriteMovies.map((movie, i) => {
+      return <p className='dataLine' key={i}>{i + 1}. <span id={`changeMovie${i + 1}`}>{movie}</span></p>
+    })
     return(
       <div>
         <div className="whiteBox">
           <div className='topWhiteBox'>
-            <h1 className='number'>{this.state.currentIndex + 1}/{this.props.data.length}</h1>
+            <h1 className='number'>{this.props.currentIndex + 1}/{this.props.data.length}</h1>
           </div>
           <div className='nameBox'>
-            <h1 className='name'>{this.props.data[this.state.currentIndex].name.first} {this.props.data[this.state.currentIndex].name.last}</h1>
+            <h1 className='name'><span id='changeFirstName'>{this.props.data[this.props.currentIndex].name.first}</span> <span id='changeLastName'>{this.props.data[this.props.currentIndex].name.last}</span></h1>
             <hr align='left'/>
             <br/>
-            <p className='dataLine'><span className='bold'>From: </span>{this.props.data[this.state.currentIndex].city}, {this.props.data[this.state.currentIndex].country}</p>
-            <p className='dataLine'><span className='bold'>Job Title:</span> {this.props.data[this.state.currentIndex].title}</p>
-            <p className='dataLine'><span className='bold'>Employer:</span> {this.props.data[this.state.currentIndex].employer}</p>
+            <p className='dataLine'><span className='bold'>From: </span><span id="changeCity">{this.props.data[this.props.currentIndex].city}</span>, <span id='changeCountry'>{this.props.data[this.props.currentIndex].country}</span></p>
+            <p className='dataLine'><span className='bold'>Job Title:</span> <span id='changeTitle'>{this.props.data[this.props.currentIndex].title}</span></p>
+            <p className='dataLine'><span className='bold'>Employer:</span> <span id='changeEmployer'>{this.props.data[this.props.currentIndex].employer}</span></p>
             <br/>
             <p className='dataLine'><span className='bold'>Favorite Movies:</span></p>
             <div className='movieList'>
@@ -50,13 +27,13 @@ export default class DataBox extends Component{
           </div>
         </div>
         <div className='buttons'>
-          <button onClick={() => this.decrementIndex(this.state.currentIndex)} className='previous'><span>&#60;</span> Previous</button>
+          <button onClick={() => this.props.decrementIndex(this.props.currentIndex)} className='previous'><span>&#60;</span> Previous</button>
             <div className='blueButtons'>
-              <button className='edit'>Edit</button>
-              <button onClick={() => this.props.delete(this.state.currentIndex)} className='edit'>Delete</button>
-              <button className='edit'>New</button>
+              <button onClick={() => this.props.editUser(this.props.currentIndex, document.getElementById('editSave').innerText)} id='editSave'>Edit</button>
+              <button onClick={() => this.props.delete(this.props.currentIndex)} className='edit'>Delete</button>
+              <button onClick={() => this.props.newUser(this.props.currentIndex, document.getElementById('editSave').innerText)} className='edit'>New</button>
             </div>
-          <button onClick={() => this.incrementIndex(this.state.currentIndex)} className='previous next'>Next <span>&#62;</span></button>
+          <button onClick={() => this.props.incrementIndex(this.props.currentIndex)} className='previous next'>Next <span>&#62;</span></button>
         </div>
       </div>
     )
